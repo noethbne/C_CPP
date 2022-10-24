84b8988a531944de4f0da11cdd112569062b2398
Bene Noeth <noeth.benedikt@gmail.com>
2022-10-24 11:54:11 +0200

    Uebung 24.10. Beginn

---
+++ addierer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1, num2, res = 0;
+    cout << "Zahl 1:    ";
+    cin >> num1;
+    cout << "Zahl 2:    ";
+    cin >> num2;
+
+    res = num1 + num2;
+
+    cout << res;
+
+    return 0;
+}
