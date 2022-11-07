dacf3861356ed5c3a2c25e34a33c6097cb381799
Bene Noeth <noeth.benedikt@gmail.com>
2022-11-07 12:51:56 +0100

    ~

--- mycars.cpp
+++ mycars.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main(){
     string mycars[4];
-    mycars[0] = "Brumm1";
-    mycars[1] = "Brumm2";
-    mycars[2] = "Brumm3";
-    mycars[3] = "Brumm4";
+    mycars[0] = "Brumm Brumm 1";
+    mycars[1] = "Brumm Brumm 2";
+    mycars[2] = "Brumm Brumm 3";
+    mycars[3] = "Brumm Brumm 4";
 
     for(string element : mycars){
         cout << element << "\n";
